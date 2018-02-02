ee794c4541a7410fc9fbfe151e4fd41cbcfdb9c5
Stephen Steinbach <abouttheday@gmail.com>
2018-02-01 23:23:23 -0800

    what a mess. Gulp dest doesn't work

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -9,19 +10,32 @@ var jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
-  'components/scripts/template.js',
+  'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style/scss'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true })
-      .on('error', gutil.log))
+      .on('error', console.log))
     .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
   gulp.src(jsSources)
   .pipe(concat('script.js'))
-  .pipe(browserify())  
+  .pipe(browserify())
   .pipe(gulp.dest('builds/development/_js'))
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+  .pipe(compass({
+    css: 'builds/development/_css', //voided the need for the .dest
+    sass: 'components/sass',
+    image: 'builds/development/_img',
+    style: 'expanded'
+  })
+  .on('error', console.log))
+  // .pipe(gulp.dest('builds/development/_css'))
 });
