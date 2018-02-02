0cdf860b652bf570dc4ac5c5a834d0eba725eb4b
Stephen Steinbach <abouttheday@gmail.com>
2018-02-02 00:12:57 -0800

    Fixed '_img' and '_css' in other files and added live reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -26,6 +27,7 @@ gulp.task('js', function() {
   .pipe(concat('script.js'))
   .pipe(browserify())
   .pipe(gulp.dest('builds/development/_js'))
+  .pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -38,6 +40,7 @@ gulp.task('compass', function() {
   })
   .on('error', console.log))
   // .pipe(gulp.dest('builds/development/_css'))
+  .pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
@@ -46,4 +49,11 @@ gulp.task('watch', function(){
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('connect', function(){
+  connect.server({
+    root: 'builds/development/',
+    livereload: true
+  })
+})
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
